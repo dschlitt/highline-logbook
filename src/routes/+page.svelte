d107f46d08a3552678e9fe4127d37b4491394ed3
Danny Schlitt <dschlitt2@gmail.com>
2025-05-18 10:26:31 -0600

    homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="container flex h-screen flex-col place-content-evenly">
+	<header class="text-center text-5xl">Webbing Logbook</header>
 
-<div class="card w-full max-w-md preset-filled-surface-100-900 p-4 text-center">
-  <p>Card</p>
+	<button type="button" class="btn preset-tonal-tertiary w-fit self-center">Add Webbing</button>
 </div>
