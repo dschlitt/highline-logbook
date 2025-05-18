db434e0988ebe814fb3dd808d4289393ca6ca965
Danny Schlitt <dschlitt2@gmail.com>
2025-05-18 08:37:48 -0600

    install skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="card w-full max-w-md preset-filled-surface-100-900 p-4 text-center">
+  <p>Card</p>
+</div>
