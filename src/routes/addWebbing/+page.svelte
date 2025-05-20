7a6cbc898b55111a5423fd1d2dada90b66e84fca
Danny Schlitt <dschlitt2@gmail.com>
2025-05-20 09:15:58 -0600

    handle forms locally

--- src/routes/addWebbing/+page.svelte
+++ src/routes/addWebbing/+page.svelte
@@ -1,7 +1,23 @@
+<script lang="ts">
+	async function handleSubmit(
+		event: SubmitEvent & { currentTarget: EventTarget & HTMLFormElement }
+	) {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+
+		//TODO: write to local storage
+		//Question: Why can't I use inspect here?
+		console.log(data);
+		for (const entry of data.entries()) {
+			console.log(entry);
+		}
+	}
+</script>
+
 <header class="m-8 text-center text-5xl">Add webbing</header>
 
 <div class="container flex h-screen flex-col place-content-evenly">
-	<form class="text-align: center w-fit" method="POST" action="?/create">
+	<form class="text-align: center w-fit" onsubmit={handleSubmit}>
 		<label>
 			Webbing Name (e.g. y2k#1)
 			<input class="text-primary-950" name="name" autocomplete="off" required />
--- src/service-worker.js
+++ src/service-worker.js
@@ -2,9 +2,5 @@ import { build } from '$service-worker';
 
 self.addEventListener('fetch', (event) => {
 	console.log('an event happened and I caught it in the service worker');
-	console.log('what what');
 	console.log(event.request.url);
-
-	// I caught the event, therefore store the stuff locallly.
-	// also cache everything starting on first load.jk
 })
