96a06a2d97e26113d396b70c75ec45b506d874fb
Danny Schlitt <dschlitt2@gmail.com>
2025-05-22 10:28:31 -0600

    calculate days rigged for each webbing

--- src/routes/webbing/+page.svelte
+++ src/routes/webbing/+page.svelte
@@ -4,6 +4,36 @@
 	import { liveQuery } from 'dexie';
 
 	let webbings = liveQuery(() => (browser ? db.webbing.toArray() : []));
+
+	// let webbingDaysAccumulator : Object & {string:number} = {};
+
+	let webbingDays = liveQuery(() => (browser ? db.rig.where({up: 0}).toArray().then(
+		(rigs) => {
+			return rigs.reduce((
+				accumulator, rig
+			) => {
+				let rigLengthDays = Math.max(
+					Math.ceil(
+						Math.abs(
+							Date.parse(rig.endDate || rig.startDate) - Date.parse(rig.startDate)
+						) / (1000 * 60 * 60 * 24)
+						), 1);
+
+				rig.webbings.forEach((webbing) => {
+					if (webbing in accumulator) {
+						accumulator[webbing] += rigLengthDays
+					}
+					else
+					{
+						accumulator[webbing] = rigLengthDays
+					}
+				});
+				return accumulator;
+			}, {});
+		}
+	) : []));
+
+	$inspect(webbingDays);
 </script>
 
 <header class="m-8 text-center text-5xl">Your Webbing</header>
@@ -14,6 +44,12 @@
 	>
 </div>
 
+{#if $webbingDays}
+	{#each $webbingDays as day}
+		<p>{day.key} : {day.value}</p>
+	{/each}
+{/if}
+
 {#if $webbings}
 	<div class="m-2 flex flex-col items-center justify-center">
 		{#each $webbings as webbing: Webbing}
@@ -23,7 +59,9 @@
 			>
 				<header class="flex justify-between">
 					<h2 class="h2 w-fit">{webbing.name}-{webbing.length}m #{webbing.segmentNumber}</h2>
-					<h3 class="h3 text-right">{webbing.backlogDays} days</h3>
+					{#if $webbingDays}
+						<h3 class="h3 text-right">{webbing.backlogDays + ($webbingDays[`${webbing.name}-${webbing.length}m#${webbing.segmentNumber}`] || 0)} days</h3>
+					{/if}
 				</header>
 				{#if webbing.notes}
 					<article class="">
@@ -36,3 +74,4 @@
 {:else}
 	<p>webbing loading ...</p>
 {/if}
+
